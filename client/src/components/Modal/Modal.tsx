d3931f214245fc006f035270bd6c8a3eac0530e9
SuRyeon-Lee <bunnyatgym@gmail.com>
2022-11-16 16:36:05 +0900

    ♻️refactor: 모달 컴포넌트 타이틀영역 버튼 종류 추가

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -1,28 +1,29 @@
 /*
 담당 : 이수련
 생성 : 2022.11.15
-수정 : -
+수정 : 2022.11.16
 소개 : 모달 창 컴포넌트
 설명 : 
   - 페이지에서 공통적으로 사용되는 모달 창 컴포넌트입니다.
   - 사용예시:
-    //배경 블러처리 있을 때 (title optional)
-    <Modal title="제목" bg={true} isOn={isPopup} setIsOn={setIsPopup}>
-      <p>내용</p>
-    </Modal>
-
-    //배경 블러처리 없을 때 (title required)
-    <Modal title="제목" bg={false} isOn={isPopup} setIsOn={setIsPopup}>
-      <p>내용</p>
-    </Modal>
-
-  - 옵션: 옵션에 대한 설명은 wiki를 참고해주세요
-  - 주의 사항: bg가 false 일땐, 반드시 title이 존재해야합니다.
+    <Modal
+        title="제목" //제목영역에 띄울 글자
+        maxWidth="700px" //L사이즈에서 최대 너비
+        bg={true} //배경 까만색 될건지
+        isOn={isPopup} //팝업 끄고 키는 state
+        setIsOn={setIsPopup} //팝업 끄고 키는 set함수
+        onTitleBtnClick={onClick} //타이틀 영역 버튼 누르면 실행되는 함수
+        titleBtn="close" //타이틀 영역 어떤 버튼이 필요한지
+      >
+  - 
 */
 
 import React from "react";
 import { Bg, Popup, PopupHeader } from "./style";
 import { ReactComponent as CloseBtn } from "../../assets/img/close-icon.svg";
+import { ReactComponent as NextBtn } from "../../assets/img/arrow-icon.svg";
+import { ReactComponent as DoneBtn } from "../../assets/img/check-icon.svg";
+import { ReactComponent as MoreBtn } from "../../assets/img/more-icon.svg";
 
 interface Prop {
   children: React.ReactNode;
@@ -32,9 +33,21 @@ interface Prop {
   title?: string;
   isOn: boolean;
   setIsOn: (arg: boolean) => void;
+  titleBtn?: "close" | "next" | "done" | "more" | "none";
+  onTitleBtnClick?: () => void;
 }
 
-const Modal = ({ children, className, maxWidth = "640px", bg = false, title = "", isOn = false, setIsOn }: Prop) => {
+const Modal = ({
+  children,
+  className,
+  maxWidth = "640px",
+  bg = false,
+  title = "",
+  isOn = false,
+  setIsOn,
+  titleBtn = "none",
+  onTitleBtnClick,
+}: Prop) => {
   const HandleOnOff = () => {
     setIsOn(!isOn);
   };
@@ -46,7 +59,15 @@ const Modal = ({ children, className, maxWidth = "640px", bg = false, title = ""
         {title ? (
           <PopupHeader title={title}>
             {title}
-            {!bg ? <CloseBtn onClick={HandleOnOff} /> : ""}
+            {
+              {
+                close: <CloseBtn onClick={HandleOnOff} />,
+                next: <NextBtn className="orange" onClick={onTitleBtnClick} />,
+                done: <DoneBtn className="orange" onClick={onTitleBtnClick} />,
+                more: <MoreBtn onClick={onTitleBtnClick} />,
+                none: "",
+              }[titleBtn]
+            }
           </PopupHeader>
         ) : (
           ""
