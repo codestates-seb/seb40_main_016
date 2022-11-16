e4ce50ad6f2a71f786e3ff6b90ced6eabdb1c2dc
ssunip <ssunip99@gmail.com>
2022-11-16 14:26:20 +0900

    :lipstick: style: header, footer css 수정

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import styled from "styled-components";
 
 const FooterContainer = styled.footer`
-  position: absolute;
-  bottom: 0;
   width: 100%;
   height: 150px;
   display: flex;
