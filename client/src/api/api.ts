4ad7b5a05e81aaa6323ceb5ad63ab3b49ecbc144
KimYunhee <qnvnfls@naver.com>
2022-11-20 17:23:41 +0900

    ✨ feat: 글 작성 type, api 추가

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,10 +1,20 @@
 import axios from "axios";
 import { SignupUserInfo, SignupPersonInfo } from "../types/user";
+import { ArticleInfo } from "../types/article";
 
 const URL = process.env.REACT_APP_URL;
 
 export const PostSignUp = async (body: SignupUserInfo | SignupPersonInfo) => {
   const response = await axios.post(`${URL}/user`, JSON.stringify(body), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return response;
+};
+
+export const SubmitNewArticle = async (body: ArticleInfo) => {
+  const response = await axios.post(`${URL}/articles`, JSON.stringify(body), {
     headers: {
       "Content-Type": "application/json",
     },
