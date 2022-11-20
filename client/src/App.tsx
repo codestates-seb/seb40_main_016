3234385f59015eb581a2971cc32acd67412b9859
KimYunhee <qnvnfls@naver.com>
2022-11-20 17:26:14 +0900

    ✨ feat: 글 작성 페이지 연동

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,20 +7,29 @@ import Main from "./pages/Main/Main";
 import Signup from "./pages/Signup/Signup";
 import Login from "./pages/Login/Signup";
 import NotFound from "./pages/NotFound/NotFound";
+import NewArticle from "./pages/Articles/NewArticle";
+import { useState } from "react";
 
 function App() {
+  const [isOn, setIsOn] = useState<boolean>(false);
+
+  const popupHandler = () => {
+    setIsOn(!isOn);
+  };
+
   return (
     <>
       <BrowserRouter>
         <ScrollToTop />
         {/* <HeaderBefore /> */}
-        <HeaderAfter />
+        <HeaderAfter popupHandler={popupHandler} />
         <Routes>
           <Route index path="/" element={<Main />} />
           <Route index path="/signup" element={<Signup />} />
           <Route index path="/login" element={<Login />} />
           <Route index path="*" element={<NotFound />} />
         </Routes>
+        <NewArticle isOn={isOn} setIsOn={setIsOn} />
       </BrowserRouter>
       <Footer />
     </>
