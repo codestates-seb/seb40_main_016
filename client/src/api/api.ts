1a23ba6e33c029168d1d8f6d615df067e7d45c55
KimYunhee <qnvnfls@naver.com>
2022-11-21 14:54:17 +0900

    ♻️ refactor: 글 관련 api 파일 분리

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { SignupUserInfo, SignupPersonInfo } from "../types/user";
-import { ArticleInfo } from "../types/article";
 
 const URL = process.env.REACT_APP_URL;
 
@@ -15,15 +14,6 @@ export const PostSignUp = async (body: SignupUserInfo | SignupPersonInfo) => {
 
 export const GetMain = async (page: number) => {
   const response = await axios.get(`${URL}/articles?page=${page}&size=12`, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  return response;
-};
-
-export const SubmitNewArticle = async (body: ArticleInfo) => {
-  const response = await axios.post(`${URL}/articles`, JSON.stringify(body), {
     headers: {
       "Content-Type": "application/json",
     },
