17693cda8cefb2e2edab742c6abd69a2ae240b59
ssunip <ssunip99@gmail.com>
2022-11-21 23:43:43 +0900

    :sparkles: feat: api 요청 코드 수정 및 토큰 추가 #57

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { SignupUserInfo, SignupPersonInfo } from "../types/user";
 
 const URL = process.env.REACT_APP_URL;
+const TOKEN = process.env.TOKEN;
 
 export const PostSignUp = async (body: SignupUserInfo | SignupPersonInfo) => {
   const response = await axios.post(`${URL}/user`, JSON.stringify(body), {
@@ -12,8 +13,18 @@ export const PostSignUp = async (body: SignupUserInfo | SignupPersonInfo) => {
   return response;
 };
 
-export const GetMain = async (page: number) => {
-  const response = await axios.get(`${URL}/articles?page=${page}&size=12`, {
+export const GetMain = async (page: number, sort: string, order: string) => {
+  const response = await axios.get(`${URL}/articles?page=${page}&sort=${sort}&order=${order}`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `${TOKEN}`,
+    },
+  });
+  return response;
+};
+
+export const GetMyPage = async (userId: number) => {
+  const response = await axios.get(`${URL}/user/${userId}`, {
     headers: {
       "Content-Type": "application/json",
     },
