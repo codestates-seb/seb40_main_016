2a7b064e809da27ccdc18690cd6fd1c73d33410d
SuRyeon-Lee <bunnyatgym@gmail.com>
2022-11-22 16:41:33 +0900

    ✨feat: textarea 높이 변환 utils 모듈 추가 #80

---
+++ client/src/utills/useAutosizeTextArea.ts
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+
+const useAutosizeTextArea = (textAreaRef: HTMLTextAreaElement | null, value: string) => {
+  useEffect(() => {
+    if (textAreaRef) {
+      textAreaRef.style.height = "0px";
+      const scrollHeight = textAreaRef.scrollHeight;
+      textAreaRef.style.height = scrollHeight + "px";
+    }
+  }, [textAreaRef, value]);
+};
+
+export default useAutosizeTextArea;
