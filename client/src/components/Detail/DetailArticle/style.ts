2253667d66b68051649321072a1e978c7a909de6
SuRyeon-Lee <bunnyatgym@gmail.com>
2022-11-22 16:44:27 +0900

    ✨feat: 글 상세 글정보 컴포넌트 1차 제작 #80

---
+++ client/src/components/Detail/DetailArticle/style.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  padding: 16px 16px 0 16px;
+  box-sizing: border-box;
+`;
+
+export const Info = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 6px;
+
+  .avatar {
+    flex-shrink: 0;
+  }
+`;
+
+export const AuthorName = styled.p`
+  width: 100%;
+  display: inline-block;
+  font-size: var(--fs-pc-large);
+  font-weight: bold;
+`;
+
+export const TimeStamp = styled.small`
+  justify-self: flex-end;
+  white-space: nowrap;
+  font-size: var(--fs-pc-small);
+  color: var(--color-gray);
+`;
+
+export const Conts = styled.div`
+  margin: 16px 0 6px;
+`;
