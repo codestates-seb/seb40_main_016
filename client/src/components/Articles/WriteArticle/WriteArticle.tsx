56ab8d38b91ce805aba92b21bc0ae00fd25a6c75
KimYunhee <qnvnfls@naver.com>
2022-11-22 16:41:04 +0900

    ♻️ refactor: 업로드된 파일의 임시주소와 파일 상태를 같이 관리할 수 있도록 수정

--- client/src/components/Articles/WriteArticle/WriteArticle.tsx
+++ client/src/components/Articles/WriteArticle/WriteArticle.tsx
@@ -1,20 +1,21 @@
 /*
 담당 : 김윤희
 생성 : 2022.11.19
-수정 : 2022.11.20
+수정 : 2022.11.22
 소개 : 글 작성 컴포넌트
 설명 : 
   - 글 작성, 수정시 사용되는 글 작성 컴포넌트입니다.
+  - 업로드된 파일의 임시주소와 파일 상태를 같이 관리할 수 있도록 수정
   - 사용 예시: <WriteArticle uploadedPhotos={uploadedPhotos} setContent={setContent} index={index} setIndex={setIndex} />
 */
 
-import { useState } from "react";
 import Avatar from "../../Avatar/Avatar";
+import { UploadedPhotos } from "../../../types/article";
 import { InnerWrapper, ArticleWrapper, SelectedPhoto, Photo, Profile, Nickname, Textarea } from "./style";
 import { ReactComponent as ArrowCircleIcon } from "../../../assets/img/arrow-circle-icon..svg";
 
 interface WriteArticleProps {
-  uploadedPhotos: string[];
+  uploadedPhotos: UploadedPhotos[];
   index: number;
   setIndex: (arg: (arg: number) => number) => void;
   setContent: (arg: () => string) => void;
@@ -26,7 +27,7 @@ const WriteArticle = ({ uploadedPhotos, index, setIndex, setContent }: WriteArti
       {uploadedPhotos.length > 0 && (
         <SelectedPhoto>
           {index > 0 && <ArrowCircleIcon className="prev" onClick={() => setIndex((index) => index - 1)} />}
-          <Photo src={uploadedPhotos[index]} alt="photo" />
+          <Photo src={uploadedPhotos[index].uploadedPhoto} alt="photo" />
           {index < uploadedPhotos.length - 1 && (
             <ArrowCircleIcon className="next" onClick={() => setIndex((index) => index + 1)} />
           )}
@@ -37,7 +38,12 @@ const WriteArticle = ({ uploadedPhotos, index, setIndex, setContent }: WriteArti
           <Avatar width="50px" height="50px" />
           <Nickname>잭슨</Nickname>
         </Profile>
-        <Textarea placeholder="글 입력" onChange={(e) => setContent(() => e.target.value)} />
+        <Textarea
+          placeholder="글 입력"
+          maxLength={500}
+          defaultValue={content}
+          onChange={(e) => setContent(() => e.target.value)}
+        />
       </ArticleWrapper>
     </InnerWrapper>
   );
