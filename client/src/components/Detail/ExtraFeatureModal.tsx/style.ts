c8527fc8389f7d238014abefb80976afb9f465aa
SuRyeon-Lee <bunnyatgym@gmail.com>
2022-11-24 16:47:54 +0900

    ✨feat: 글,댓글 moreBtn 클릭시 나오는 팝업 컴포넌트 제작 #80

---
+++ client/src/components/Detail/ExtraFeatureModal.tsx/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  padding: 16px;
+
+  button {
+    margin-bottom: 10px;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+`;
