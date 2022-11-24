1e82e1be25fbc7e665ca53fa47d2bd1fb6d1000e
KimYunhee <qnvnfls@naver.com>
2022-11-24 10:02:28 +0900

    ✨ feat: shop type, api 추가

---
+++ client/src/api/shop.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { OrderItemsProps } from "../types/shop";
+
+const URL = process.env.REACT_APP_URL;
+
+export const GetItems = async (token: string) => {
+  const response = await axios.get(`${URL}/items`, {
+    headers: { Authorization: `${token}` },
+  });
+
+  return response.data;
+};
+
+export const OrderItems = async (body: OrderItemsProps[], token: string) => {
+  const response = await axios.post(`${URL}/wallet`, JSON.stringify({ orderItems: body }), {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `${token}`,
+    },
+  });
+
+  return response.data;
+};
