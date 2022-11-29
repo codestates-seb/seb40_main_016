5bb643931a0d071d79bf0c0e278cd8e21bec1c60
KimYunhee <qnvnfls@naver.com>
2022-11-29 17:40:18 +0900

    ✨ feat: 회원탈퇴 API 변경

--- client/src/api/user.ts
+++ client/src/api/user.ts
@@ -55,16 +55,6 @@ export const PatchProfile = async (userId: number, formData: FormData, token: st
   return response;
 };
 
-export const DeleteUser = async (userId: number, token: string) => {
-  const response = await axios.delete(`${URL}/user/${userId}`, {
-    headers: {
-      Authorization: `${token}`,
-    },
-  });
-
-  return response;
-};
-
 export const CheckPassword = async (token: string, password: string) => {
   const response = await axios.post(`${URL}/user/passcheck`, JSON.stringify({ password: password }), {
     headers: {
