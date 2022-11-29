c6f9a95b5b17475cccc3734bafdd3955ffe50e2f
kugnas <sanguk821@gmail.com>
2022-11-30 01:35:31 +0900

    ✨ feat: 검색기능 구현(v2) ♻️ refactor: 글조회 queryDsl을 사용하도록 리팩토링

--- server/src/main/java/com/cocoa/catdog/article/repository/ArticleRepositoryCustom.java
+++ server/src/main/java/com/cocoa/catdog/article/repository/ArticleRepositoryCustom.java
@@ -4,6 +4,10 @@ import com.cocoa.catdog.article.entity.Article;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface ArticleRepositoryCustom {
-    Page<Article> findBySearch(Pageable pageable, String sort, String q);
+    Page<Article> findBySearch(Pageable pageable, String sort, String search);
+
+    Page<Article> findBySearchAndFollowing(Pageable pageable, List<Long> followedUsers, String search);
 }
