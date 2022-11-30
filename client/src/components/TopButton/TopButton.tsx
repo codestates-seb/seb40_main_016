78efaadd5301fa974b55d4cfdd448f59b661c857
ssunip <ssunip99@gmail.com>
2022-11-30 16:12:48 +0900

    :sparkles: feat: 메인페이지 top 버튼 구현

---
+++ client/src/components/TopButton/TopButton.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const TopButtonContainer = styled.div`
+  position: fixed;
+  right: 4%;
+  bottom: 7%;
+  z-index: 1;
+
+  @media screen and (max-width: 736px) {
+    right: 6%;
+    bottom: 3%;
+  }
+`;
+
+const Top = styled.button`
+  width: 60px;
+  height: 60px;
+  background-color: white;
+  box-shadow: 0px 2px 10px 0px var(--color-gray);
+  border: none;
+  border-radius: 40px;
+
+  &:hover {
+    background-color: var(--color-ivory);
+    cursor: pointer;
+  }
+
+  @media screen and (max-width: 736px) {
+    width: 45px;
+    height: 45px;
+  }
+`;
+
+const TopButton = () => {
+  const [showButton, setShowButton] = useState<Boolean>(false);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    const handleShowButton = () => {
+      if (window.screenY > 20) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+    console.log(window.screenY);
+    window.addEventListener("scroll", handleShowButton);
+    return () => {
+      window.removeEventListener("scroll", handleShowButton);
+    };
+  }, []);
+
+  return (
+    showButton && (
+      <TopButtonContainer>
+        <Top onClick={scrollToTop} type="button">
+          Top
+        </Top>
+      </TopButtonContainer>
+    )
+  );
+};
+
+export default TopButton;
