b40da4836d907432bd809021ae479ecfa241f1c7
SuRyeon-Lee <bunnyatgym@gmail.com>
2022-12-02 08:23:38 +0900

    ♻️refactor: 헷갈릴 수 있는 비밀번호 에러메세지 수정 #166

--- client/src/components/Signup/SignupFormStep/StepForm.tsx
+++ client/src/components/Signup/SignupFormStep/StepForm.tsx
@@ -65,7 +65,7 @@ const StepForm = ({ userInfo, setUserInfo, setHasNoError }: Prop) => {
             fontSize="pc-regular"
             label="닉네임"
             isError={userNameErr}
-            errorMsg="한글, 영문, 숫자 조합 2-10자리로 입력해 주세요."
+            errorMsg="2-10자리로 입력해 주세요.(한글, 영문, 숫자 가능)"
           />
         ) : (
           <Input
@@ -78,7 +78,7 @@ const StepForm = ({ userInfo, setUserInfo, setHasNoError }: Prop) => {
             fontSize="pc-regular"
             label="이름"
             isError={userNameErr}
-            errorMsg="한글, 영문, 숫자 조합 2-10자리로 입력해 주세요."
+            errorMsg="2-10자리로 입력해 주세요.(한글, 영문, 숫자 가능)"
           />
         )}
 
